75a8fdc283bcfddd81775847edbb1fd8c435f73f
Nikhil Kulkarni <nikhilskul7@gmail.com>
2024-09-26 22:58:51 -0400

    Day  5

---
+++ Strings and Arrays/2525. Categorize Box According to Criteria.py
@@ -0,0 +1,18 @@
+class Solution:
+    def categorizeBox(self, length: int, width: int, height: int, mass: int) -> str:
+        bulky=False
+        heavy=False
+        if length>= 10**4 or width>= 10**4 or height>= 10**4 or mass>= 10**4 or (length*width*height)>=10**9:
+            bulky=True
+        
+        if mass>=100:
+            heavy=True
+        
+        if bulky and heavy:
+            return "Both"
+        elif not bulky and not heavy:
+            return "Neither"
+        elif bulky and not heavy:
+            return "Bulky"
+        else:
+            return "Heavy"
