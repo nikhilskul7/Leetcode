731240fb4f120a22a78531f941b8ca8f7ac9f890
Nikhil Kulkarni <nikhilskul7@gmail.com>
2024-10-03 17:51:16 -0400

    Day 10

---
+++ Strings and Arrays/66. Plus One.py
@@ -0,0 +1,21 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        
+        
+        carry=0
+        
+        
+        for i in range(len(digits)-1,-1,-1):
+            if digits[i]==9:
+                digits[i]=0
+                carry=1
+                
+            else:
+                digits[i]+=1
+                carry=0
+                break
+                
+        if carry==1:
+            digits.insert(0,1)
+            
+        return digits
