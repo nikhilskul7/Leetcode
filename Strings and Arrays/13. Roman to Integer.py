92c8b3384892e5a28e1838e69b918e4c83226c37
Nikhil Kulkarni <nikhilskul7@gmail.com>
2024-10-06 15:10:28 -0400

    Day 13

---
+++ Strings and Arrays/13. Roman to Integer.py
@@ -0,0 +1,21 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        m = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+        
+        retval=0
+        
+        for i in range(len(s)):
+            if i+1<len(s) and m[s[i+1]]>m[s[i]]:
+                retval-=m[s[i]]
+            else:
+                retval+=m[s[i]]
+        
+        return retval
