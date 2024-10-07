9b3a8854260cca65afe391022f7868e6ad07a70f
Nikhil Kulkarni <nikhilskul7@gmail.com>
2024-10-06 22:47:43 -0400

    Day 14

---
+++ Trees/938. Range Sum of BST.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
+        self.retval=0
+        
+        def check(root):
+            if not root:
+                return
+            
+            if low<=root.val<=high:
+                self.retval+=root.val
+                
+            check(root.left)
+            check(root.right)
+            
+        check(root)
+        
+        return self.retval
